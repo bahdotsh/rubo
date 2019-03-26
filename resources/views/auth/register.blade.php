085de8d721ea344bbbd98d3261a5fd1e0c9e0dd8
bohemianjock <appu.yess@gmail.com>
2019-03-26 17:00:32 +0530

    coach signup

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -62,6 +62,11 @@
                         </div>
 
                         <div class="form-group">
+                          <a href = "/coach/register"> Sign Up </a> as an Expert 
+
+                        </div>
+
+                        <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
                                     Register
